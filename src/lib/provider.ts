9f496ff27e33597d0d63a2d2fa7f544eac95dbbf
Fernando Lopez <37802572+Cainuriel@users.noreply.github.com>
2025-01-12 16:25:24 +0100

    Update provider.ts

--- src/lib/provider.ts
+++ src/lib/provider.ts
@@ -1,11 +1,75 @@
 import { ethers } from 'ethers'
-
+import currentConfig from './config/currentConfig.json'
 // Initialize provider outside component
-export const provider = new ethers.JsonRpcProvider('https://data-seed-prebsc-1-s1.binance.org:8545/')
+// export const provider = new ethers.JsonRpcProvider('https://data-seed-prebsc-1-s1.binance.org:8545/')
+const TOKEN_ABI = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function balanceOf(address) view returns (uint)",
+  
+  ]
+class Web3Provider {
+	private provider: ethers.JsonRpcProvider;
+    private contractPFX: string;
+    private contractUSDT: string;
+    private contractUSDC: string;
 
-// export const contractPFXProvider =  async() =>  {
+	constructor() {
+		this.provider = new ethers.JsonRpcProvider(currentConfig.nodeUrl);
+        this.contractPFX = currentConfig.polfexContract;
+        this.contractUSDT = currentConfig.usdtContract;
+        this.contractUSDC = currentConfig.usdcContract;
+	}
 
-//     const contractPFX = "0x1b3f1f4f96d2c0e1d0a7e3c4d9f3f5c2b3b3f3c7";
-// 	const gameContract = new ethers.Contract(contractPFX, GameABI.abi, provider);
-// 	return gameContract;
-// }
+	async balanceNativeToken(address: string): Promise<string> {
+		const balance = await this.provider.getBalance(address);
+		return ethers.formatEther(balance)
+	}
+
+
+    async pfxToken(address: string): Promise< [string, string, ethers.BigNumberish] > {
+
+		const pfxContract =  new ethers.Contract(this.contractPFX, TOKEN_ABI, this.provider);
+        const name = await pfxContract.name();
+
+        const symbol = await pfxContract.symbol();
+
+        const balance = await pfxContract.balanceOf(address);
+
+        return [ name, symbol, balance];
+	}
+
+    async usdtoken(address: string): Promise<[string, string, ethers.BigNumberish] > {
+
+		const pfxContract =  new ethers.Contract(this.contractUSDT, TOKEN_ABI, this.provider);
+        const name = await pfxContract.name();
+        const symbol = await pfxContract.symbol();
+
+        const balance = await pfxContract.balanceOf(address);
+        return [ name, symbol, balance];
+	}
+
+    async usdcToken(address: string): Promise<[string, string, ethers.BigNumberish] > {
+
+		const pfxContract =  new ethers.Contract(this.contractUSDC, TOKEN_ABI, this.provider);
+        const name = await pfxContract.name();
+
+        const symbol = await pfxContract.symbol();
+ 
+        const balance = await pfxContract.balanceOf(address);
+    
+        return [ name, symbol, balance];
+	}
+
+	async recoverSigner(hash: string, signature: string): Promise<string> {
+	
+		return ethers.recoverAddress(hash, signature)
+	}
+}
+
+// Usage example:
+// const web3Provider = new Web3Provider();
+// const balance = await web3Provider.balanceToken('0x...');
+// const contract = await web3Provider.firmaAcuerdos('0x...', GameABI.abi, signer);
+
+export const web3Provider = new Web3Provider();
